82e048ccd252950243078f8049c8dbdb3b51461b
Paulo Conde <pauloconde@gmail.com>
2024-02-19 22:22:41 -0400

    Initial Commit

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font'
+import { useEffect } from 'react';
 
 export default function App() {
+
+  const [loaded] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      // can hide splash screen here
+    }
+  }, [loaded])
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>DominoScore</Text>
       <StatusBar style="auto" />
     </View>
   );
